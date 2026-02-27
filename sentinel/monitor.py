6654aa8c995e1294b1a3ce1bb549b018467aeacf
Shruti Banerjee <85667186+nocturnal-creature@users.noreply.github.com>
2026-02-27 20:32:16 +0530

    monitor.py

---
+++ sentinel/monitor.py
@@ -0,0 +1,49 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from .hashing import calculate_hash
+from .baseline import load_baseline
+from .logger import log_event
+import time
+
+class SentinelHandler(FileSystemEventHandler):
+    def __init__(self, baseline_file, log_file):
+        self.baseline_file = baseline_file
+        self.log_file = log_file
+
+    def on_modified(self, event):
+        if not event.is_directory:
+            self.check_file(event.src_path)
+
+    def on_created(self, event):
+        if not event.is_directory:
+            log_event(f"[NEW FILE] {event.src_path}", self.log_file)
+
+    def on_deleted(self, event):
+        if not event.is_directory:
+            log_event(f"[DELETED] {event.src_path}", self.log_file)
+
+    def check_file(self, filepath):
+        baseline = load_baseline(self.baseline_file)
+        current_hash = calculate_hash(filepath)
+
+        if filepath in baseline:
+            if baseline[filepath] != current_hash:
+                log_event(f"[MODIFIED] {filepath}", self.log_file)
+        else:
+            log_event(f"[UNKNOWN FILE] {filepath}", self.log_file)
+
+def start_monitoring(directory, baseline_file, log_file):
+    event_handler = SentinelHandler(baseline_file, log_file)
+    observer = Observer()
+    observer.schedule(event_handler, directory, recursive=True)
+    observer.start()
+
+    print("Monitoring started...")
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
